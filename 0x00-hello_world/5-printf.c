4db7eab711a1e9dbd1d7bed4397230d26a321af7
Darkcoffee1859 <shency49@gmail.com>
2023-02-17 01:34:20 +0100

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints a line using printf function
+ * Return: (0) Success
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome ia a piece of art\n,")
+		return (0);
+}
