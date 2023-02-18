fbe516d57c9f0054e27197c368e32d655869a6a2
Patience <shency49@gmail.com>
2023-02-18 11:29:54 -0800

    corrention print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - print a c program using printf
- * Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	retuen (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
