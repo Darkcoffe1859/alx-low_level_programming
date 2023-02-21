c04e2d1313897d35e36fb0dbc7eb2c27a9e31eac
Patience <shency49@gmail.com>
2023-02-21 13:04:02 -0800

    a program that prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
-#include "-putchar"
+*include "main.h"
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints _putchar,
+ * followed by a new line
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-	char *sh = "-putchar";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u);
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
