245da671824ba77212136eb5cb1fa50babc69206
Patience <shency49@gmail.com>
2023-02-23 10:14:50 -0800

    more function nested loops

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+this is a project during my studies with alx software engineering course
+this project is centered on C prograaming which is a low level language programming
+this project focus on more functions, nested loops and it has a prototype and a main.h file in it
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN.H
+#define MAIN_H
+
+int _putchar (Char c);
+int _isupper (int c);
+int _isdigit (int c);
+int mul(int a, int b);
+void print_numbers (void);
+void print_most_numbers (void);
+void more_numbers (void);
+void print_diagonal (int c);
+void print_square (int size);
+voi print_triangle (int sixe);
+#endif
