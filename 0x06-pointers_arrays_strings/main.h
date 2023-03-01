7e97a5da253472ef18f7c03396a3836721d70847
Patience <shency49@gmail.com>
2023-03-01 13:58:08 -0800

    Infinite add

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,5 @@ char *cap_string(char *n);
 char *leet(char *n);
 char *rot13(char *n);
 void print_number (int n);
-char *magic(char *n)
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif /* HOLBERTON_H */
