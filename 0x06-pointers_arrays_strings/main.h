77a090105e28464384a858ad4bae92d0494a9a24
Patience <shency49@gmail.com>
2023-03-01 14:08:46 -0800

    print buffer edited

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,4 +13,6 @@ char *leet(char *n);
 char *rot13(char *n);
 void print_number (int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
 #endif /* HOLBERTON_H */
