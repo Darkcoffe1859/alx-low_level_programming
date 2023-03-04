90b08152f838e36f417508b7b8f651b4920c82e2
Patience <shency49@gmail.com>
2023-03-04 13:30:37 -0800

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - Prints all possible combinations of single-digit numbers.
+ * main - The entry point of my program
  *
- * Return: Always 0.
+ * Description: The program prints all single digit numbers
+ * of base 10 starting from 0, followed by a new line.
+ *
+ * Return: Return O if there is no error in main program
  */
 int main(void)
 {
-	int num;
+	int n = 0;
 
-	for (num = 0; num <= 9; num++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
+		putchar((char) n);
 	}
-
 	putchar('\n');
 
 	return (0);
