79f4039b9296c452e9328ea3ca6886e3a44fbd56
Patience <shency49@gmail.com>
2023-03-04 11:59:54 -0800

    0-stracat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,13 +6,11 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int *n);
-char *string_toupper(char n);
+void reverse_array(int *a, int n);
+char *string_toupper(char *n);
 char *cap_string(char *n);
 char *leet(char *n);
 char *rot13(char *n);
-void print_number (int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
-int _magic(char *a, char *p;)
-#endif /* HOLBERTON_H */
+
+#endif /* MAIN_H */
+
