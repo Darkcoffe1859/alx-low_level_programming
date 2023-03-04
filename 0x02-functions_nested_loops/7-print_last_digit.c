392ba88b3cb9b1de85ff00b5d4224ed5423e9d29
Patience <shency49@gmail.com>
2023-03-04 14:00:59 -0800

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,26 +1,26 @@
 #include "main.h"
-/**
- * jack_bauer - function that prints every minute of the day, 24 hour clock
- * h = hour, m = minutes
- * / 10 allows second digit to rotate
- * for loop breaks before passing 24:00
- * Return: 24 hour clock line by line
- */
-void jack_bauer(void)
-{
-	int h, m;
 
-	for (h = 0; h < 24; h++)
+/**
+ * print_last_digit - A program that prints the last digit
+ * of a number.
+ *
+ * @n: input number as an integer.
+ *
+ * Return: last digit.
+ */
+int print_last_digit(int n)
+{
+	int numb;
+
+	numb = n % 10;
+	if (numb < 0)
 	{
-		for (m = 0; m < 60; m++)
-		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
-			_putchar(':');
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
-			_putchar('\n');
-		}
+		_putchar(-numb + '0');
+		return (-numb);
+	}
+	else
+	{
+		_putchar(numb + '0');
+		return (numb);
 	}
 }
-
