50b1c1444aa4c87ae75ee04cd53de563b25af7c0
Patience <shency49@gmail.com>
2023-03-04 12:26:05 -0800

    pointers and arrays

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,6 +11,8 @@ char *string_toupper(char *n);
 char *cap_string(char *n);
 char *leet(char *n);
 char *rot13(char *n);
-
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
+void print_buffer(char *b, int size);
 #endif /* MAIN_H */
 
