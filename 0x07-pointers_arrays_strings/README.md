e486395bcf10e88e36d3b325bdae31243f41f9e1
Patience <shency49@gmail.com>
2023-03-06 04:27:21 -0800

    main.h

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this project is focused on pointers, arrays and strings
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char);
+char *_memset(char *, char , unsigned int);
+char *_memcpy(char *, char *, unsigned int);
+char *_strchr(char *, char);
+unsigned int _strspn(char *, char *);
+char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
+void print_chessboard(char (*)[]);
+void print_diagsums(int *, int);
+void set_string(char **, char *);
+int strleng (char *);
+
+
+#endif /* MAIN_H */
