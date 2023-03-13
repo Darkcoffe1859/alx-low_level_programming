1907fad0f11a529b677270ab73c57f720aafcef0
Patience <shency49@gmail.com>
2023-03-13 13:52:03 -0700

    main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,11 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char *c);
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 char *create_array(unsigned int size, char c);
-char *_strdup(char *s1, char *s2);
-char *str_concat(char *s1, char *s2)
-int **alloc_grid (int width, int height);
-void free_grid(int ** grid, int height);
-char *argstorstr(int ac, char **av);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 char **strtow(char *str);
+int _putchar(char *c)
+
 #endif
